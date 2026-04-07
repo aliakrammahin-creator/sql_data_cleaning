93cfbf60bb46faa3cf8fc8f9f55560c18ed5a33c
aliakrammahin-creator <aliakrammahin@gmail.com>
2026-04-07 16:37:14 +0600

    Add files via upload

---
+++ sql_data_cleaning_layoffs.sql
@@ -0,0 +1,113 @@
+use world_layoffs;
+select * from layoffs;
+
+-- ## Removing duplicates
+create table layoff_staging
+like layoffs;
+insert into layoff_staging 
+select * from layoffs;
+
+with duplicate_cte as
+(
+select *,
+row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off,
+`date`, stage, country, funds_raised_millions) as row_num
+from layoff_staging)
+select * 
+from duplicate_cte
+where row_num > 1;
+
+CREATE TABLE `layoff_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert into layoff_staging2
+select *,
+row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off,
+`date`, stage, country, funds_raised_millions) as row_num
+from layoff_staging;
+select * from layoff_staging2 where row_num > 1;
+set sql_safe_updates = 0;
+delete from layoff_staging2 where row_num > 1;
+
+-- ##Standarizing data
+select * from layoff_staging2;
+select company, trim(company)
+from layoff_staging2;
+update layoff_staging2
+set company = trim(company);
+
+select distinct industry from layoff_staging2 order by 1;
+select * from layoff_staging2 where industry like 'Crypto%';
+update layoff_staging2
+set industry = 'Crypto'
+where industry like 'Crypto%';
+
+select * from layoff_staging2
+where country like 'United States%';
+update layoff_staging2
+set country = 'United States'
+where country like 'United States%';
+
+select `date`,
+str_to_date(`date`, '%m/%d/%Y') from layoff_staging2;
+update layoff_staging2
+set `date` = str_to_date(`date`, '%m/%d/%Y');
+alter table layoff_staging2
+modify column `date` date;
+
+-- ## null values and blank values
+select *
+from layoff_staging2
+where industry is null
+or industry ='';
+select * 
+from layoff_staging2
+where company = 'Airbnb';
+
+update layoff_staging2
+set industry = null 
+where industry = '';
+
+select t1.industry, t2.industry
+from layoff_staging2 t1
+join layoff_staging2 t2
+     on t1.company = t2.company
+     and t1.location = t2.location
+where (t1.industry is null or t1.industry = '')
+and t2.industry is not null;
+
+update layoff_staging2 t1
+join layoff_staging2 t2
+     on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null
+and t2.industry is not null;
+
+-- ## removing columns
+select * 
+from layoff_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+set sql_safe_updates = 0;
+
+delete
+from layoff_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select *
+from layoff_staging2;
+alter table layoff_staging2
+drop column row_num;
+select *
+from layoff_staging2;
